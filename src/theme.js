6f3f7ac02c4dce1323dde13d392efcd9993ad53b
pvdoi <phanvangioi1361@gmail.com>
2024-05-16 14:58:38 +0700

    Update Board Content P2

--- src/theme.js
+++ src/theme.js
@@ -4,14 +4,17 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 const APP_BAR_HEIGHT = '58px'
 const BOARD_BAR_HEIGHT = '60px'
 const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
-
+const COULMN_HEADER_HEIGHT = '50px'
+const COULMN_FOOTER_HEIGHT = '56px'
 
 // Create a theme instance.
 const theme = extendTheme({
   trello: {
     appBarHeight: APP_BAR_HEIGHT,
     boardBarHeight: BOARD_BAR_HEIGHT,
-    boardContentHeight: BOARD_CONTENT_HEIGHT
+    boardContentHeight: BOARD_CONTENT_HEIGHT,
+    columnHeaderHeight: COULMN_HEADER_HEIGHT,
+    columnFooterHeight: COULMN_FOOTER_HEIGHT
   },
   colorSchemes: {
     light: {},
