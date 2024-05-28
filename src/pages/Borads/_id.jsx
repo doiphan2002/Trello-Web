b297023cfdd74bcf1d8fb13f120a37797652aac1
pvdoi <phanvangioi1361@gmail.com>
2024-05-28 14:33:45 +0700

    Update Button Create

--- src/pages/Borads/_id.jsx
+++ src/pages/Borads/_id.jsx
@@ -2,15 +2,16 @@ import { Container } from '@mui/material'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
 import { useEffect, useState } from 'react'
+
+import { mockData } from '~/apis/mock-data'
 import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
   const [board, setBoard] = useState(null)
 
   useEffect(() => {
-    const boardId = '66543edf5e2622aa70d8d358'
+    const boardId = '66555f0fe09cdbc7e6bed3ec'
     // Call API
     fetchBoardDetailsAPI(boardId).then(board => {
       setBoard(board)
@@ -21,8 +22,8 @@ function Board() {
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={board} />
-      <BoardContent board={board} />
+      <BoardBar board={mockData?.board} />
+      <BoardContent board={mockData?.board} />
     </Container>
   )
 }
