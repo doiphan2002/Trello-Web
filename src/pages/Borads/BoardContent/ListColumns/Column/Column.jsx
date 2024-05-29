2a233b9a65ec1e6e2343441d713ab5ff7ce78f9c
pvdoi <phanvangioi1361@gmail.com>
2024-05-29 10:44:53 +0700

    Custome Libs

--- src/pages/Borads/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Borads/BoardContent/ListColumns/Column/Column.jsx
@@ -182,6 +182,7 @@ function Column({ column }) {
                 size="small"
                 variant="outlined"
                 autoFocus
+                data-no-dnd= "true"
                 value={newCardTitle}
                 onChange={(e) => setNewCardTitle(e.target.value)}
                 sx={{
